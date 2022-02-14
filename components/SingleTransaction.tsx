d6c693b431f422a9151877072f3c281ae2d5058b
yovany-lg <yovany.lg@gmail.com>
2022-02-14 05:42:17 -0600

    Adding transaction view

--- components/SingleTransaction.tsx
+++ components/SingleTransaction.tsx
@@ -1,4 +1,5 @@
 import { ArrowNarrowUpIcon, ArrowNarrowDownIcon } from "@heroicons/react/solid";
+import Link from "next/link";
 import { FC } from "react";
 import { Transaction, TransactionType } from "../types/transactions.types";
 
@@ -8,7 +9,7 @@ interface SingleTransactionProps {
 
 const SingleTransaction: FC<SingleTransactionProps> = ({ transaction }) => {
   // extract the Transaction props
-  const { type, description, amount, date } = transaction;
+  const { type, description, amount, date, id } = transaction;
   const isIncome = type === TransactionType.income;
   return (
     <div className="flex justify-between py-4">
@@ -16,7 +17,9 @@ const SingleTransaction: FC<SingleTransactionProps> = ({ transaction }) => {
         ? <ArrowNarrowUpIcon className="h-8 w-8 py-1 px-2 rounded-full bg-green-100 text-green-800" />
         : <ArrowNarrowDownIcon className="h-8 w-8 py-1 px-2 rounded-full bg-red-200 text-red-800" />}
       <div className="flex flex-grow flex-col px-8">
-        <span className="font-medium">{description}</span>
+        <Link href={`transaction/${id}`} passHref>
+          <a className="font-medium">{description}</a>
+        </Link>
         <span className="mt-1 text-gray-500 text-sm">{date.toDate().toDateString()}</span>
       </div>
       <div className={isIncome ? 'text-green-500' : 'text-red-600'}>${amount.toFixed(2)}</div>
--- lib/transactions.ts
+++ lib/transactions.ts
@@ -1,4 +1,4 @@
-import { addDoc, collection, doc, getDocs, setDoc, Timestamp } from "firebase/firestore";
+import { addDoc, collection, doc, getDoc, getDocs, setDoc, Timestamp } from "firebase/firestore";
 import { FormInput, Transaction } from "../types/transactions.types";
 import { db } from './firebase'
 
@@ -26,4 +26,16 @@ export async function saveTransaction(data: FormInput): Promise<void> {
   } catch (error) {
     console.error(error)
   }
+}
+
+export async function getTransaction(id: string): Promise<Transaction> {
+  const docRef = doc(db, 'transactions', id);
+  const docSnapshot = await getDoc(docRef);
+  let data: any;
+  if (docSnapshot.exists()) {
+    data = docSnapshot.data() as Transaction;
+  } else {
+    console.log('Sorry, no data for this ID');
+  }
+  return data;
 }
