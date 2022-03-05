31e839b7d975de3768556274d6636db19e5b043b
Yovany Luis <yovany.lg@gmail.com>
2022-03-05 08:28:12 -0600

    eslint and prettier

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,23 +1,23 @@
-import { useRouter } from "next/router";
-import { FC } from "react";
+import { useRouter } from 'next/router'
+import { FC } from 'react'
 
 interface LoginPageProps {
-  signIn: (cb: () => void) => void;
+  signIn: (cb: () => void) => void
 }
 
 const LoginPage: FC<LoginPageProps> = ({ signIn }) => {
-  const router = useRouter();
+  const router = useRouter()
   const onSuccess = () => {
-    router.push('/');
+    router.push('/')
   }
   return (
     <button
       className="hover:bg-emerald-600 bg-emerald-500 text-white px-5 py-2.5 text-base leading-5 rounded-md font-medium border"
       onClick={() => signIn(onSuccess)}
     >
-      Sign In
+      Sign in
     </button>
   )
 }
 
-export default LoginPage;
+export default LoginPage
